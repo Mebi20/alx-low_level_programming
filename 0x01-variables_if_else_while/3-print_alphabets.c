e9c70bee43c2ebc8730c577a262b976502037838
Mebi20 <osuborchukwudumebi@gmail.com>
2022-04-08 17:17:55 -0700

    go

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,10 @@ int main(void)
 {
  int ch;
 
- for (ch = 'a'; ch <= 'z'; ch++)   
-  putchar(ch);
+ for (ch = 'a'; ch <= 'z'; ch++)
+       	 putchar(ch);
  for (ch = 'A'; ch <= 'Z'; ch++)
   putchar(ch);
 putchar('\n');
 return (0);
-}
-© 2022 GitHub, 
+} 
