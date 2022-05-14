0c82ea54a491139c0eb1f211fc41be7326ffd72e
Mebi20 <osuborchukwudumebi@gmail.com>
2022-05-13 17:13:09 -0700

    go

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,15 +1,11 @@
-#ifndef FUNC_POINTERS
-#define FUNC_POINTERS
-/*
-* File: function_pointers.h
-* Author: Osubor Chukwudumebi
-*/
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
 
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif /*FUNC_POINTERS*/
+#endif /* _FUNCTION_POINTERS_H_ */
